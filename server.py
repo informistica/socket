44752eee35887049ebfb7005d16b28d9d04cba28
informistica <informistica>
2021-01-22 18:41:45 +0100

    Rinominate socket

--- server.py
+++ server.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
+
+
+#premessa sul formato di dati scambiato
+input_string = 'Hello'
+print(type(input_string))
+input_bytes_encoded = input_string.encode()
+print(type(input_bytes_encoded))
+print(input_bytes_encoded)
+output_string=input_bytes_encoded.decode()
+print(type(output_string))
+print(output_string)
+
 #https://realpython.com/python-sockets/
 import socket
 
 HOST = '127.0.0.1'  # Indirizzo dell'interfaccia standard di loopback (localhost)
 PORT = 65432        # Porta di ascolto, la lista di quelle utilizzabili parte da >1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock_listen:
+    sock_listen.bind((HOST, PORT))
+    sock_listen.listen()
     print("[*] In ascolto su %s:%d" % (HOST, PORT))
-    clientsocket, address = s.accept()
-    with clientsocket as cs:
-        print('Connessione da', address)
+    sock_service, address_client = sock_listen.accept()
+    with sock_service as ss:
+        print('Connessione da', address_client)
         while True:
-            data = cs.recv(1024)
+            data = ss.recv(1024)
             if not data:
                 break
-            cs.sendall(data)
+            ss.sendall(data)
