a74af3d3beac3627851c8b81cb431241c94a0b1b
informistica <informistica>
2021-01-22 18:32:40 +0100

    rinominate socket

--- client.py
+++ client.py
@@ -2,21 +2,38 @@
 
 import socket
 
-SERVER_ADDRESS = '127.0.0.1'  # The server's hostname or IP address
-SERVER_PORT = 65433        # The port used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((SERVER_ADDRESS, SERVER_PORT))
-dati = input("Inserisci messaggio per il server: ")
-dati = dati.encode()
-# Send data to server
-s.send(dati)
-# Receive response from server
-dati = s.recv(2048)
-if dati:
-    # Convert back to string for python3
+SERVER_ADDRESS = '127.0.0.1'
+SERVER_PORT = 22224
+
+sock_service = socket.socket()
+
+sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
+
+print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+while True:
+    try:
+        dati = input("Inserisci i dati da inviare: ")
+    except EOFError:
+        print("\nOkay. Exit")
+        break
+
+    if not dati:
+        print("Non puoi inviare una stringa vuota!")
+        continue
+
+    dati = dati.encode()
+
+    sock_service.send(dati)
+
+    dati = sock_service.recv(2048)
+
+    if not dati:
+        print("Server non risponde. Exit")
+        break
+
     dati = dati.decode()
-    print("Ho ricevuto dal server: ")
+
+    print("Ricevuto dal server:")
     print(dati + '\n')
 
-
-
+sock_service.close()
--- server.py
+++ server.py
@@ -1,31 +1,68 @@
 #!/usr/bin/env python3
-#https://realpython.com/python-sockets/
 import socket
 
-HOST = '127.0.0.1'  # Indirizzo dell'interfaccia standard di loopback (localhost)
-PORT = 65432        # Porta di ascolto, la lista di quelle utilizzabili parte da >1023)
+# lasciando il campo vuoto sarebbe la stessa cosa (localhost)
+SERVER_ADDRESS = '127.0.0.1'
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Numero di porta, deve essere >1024 perchè le altre sono riservate.)
+SERVER_PORT = 22224
 
-# Optionale: permette di riavviare subito il codice,
+# Crea la socket
+sock_listen = socket.socket()
+op = " "
+n1 = 0
+n2 = 0
+# Opzionale: permette di riavviare subito il codice,
 # altrimenti bisognerebbe aspettare 2-4 minuti prima di poter riutilizzare(bindare) la stessa porta
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-s.bind((HOST, PORT))
-s.listen()
-print("[*] In ascolto su %s:%d" % (HOST, PORT))
-clientsocket, address = s.accept()
-with clientsocket as cs:
-    print('Connessione da', address)
+# Associa indirizzo e porta.  Nota che  l'argumento è una tupla:
+sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
+
+# Imposta quante connessioni pendenti possono essere accodate
+sock_listen.listen(5)
+
+print("Server in ascolto su %s. Termina con ko" % str((SERVER_ADDRESS, SERVER_PORT)))
+
+# Abbiamo creato un endpoint di ascolto (sock_listen) dal quale accettare connessioni in entrata
+# Il loop accetterà una connessione alla volta e la servirà in una nuova connessione (sock_service) finchè il client non si disconnette
+# dopodichè il server ne accetterà di nuove tra quelle in coda
+
+while True:
+    sock_service, addr_client = sock_listen.accept()
+    print("\nConnessione ricevuta da %s" % str(addr_client))
+    print("\nAspetto di ricevere i dati dell'operazione ")
+
     while True:
-        dati = cs.recv(1024)
-        dati.decode()
+        dati = sock_service.recv(2048)
         if not dati:
+            print("Fine dati dal client. Reset")
             break
+
+        # Decodifica i byte ricevuti in una stringa unicode
         dati = dati.decode()
-        print("Ricevuto '%s' dal client" % dati)
-        dati = "Ciao, " + str(address) + ". Ho ricevuto questo: '" + dati + "'"
+        if dati == "ko":
+            print("Fine dati dal client. Exit")
+            break
+        op, n1, n2 = dati.split(";")
+        if op == "piu":
+            dati = str(float(n1) + float(n2))
+        elif op == "meno":
+            dati = str(float(n1) - float(n2))
+        elif op == "per":
+            dati = str(float(n1) * float(n2))
+        elif op == "diviso":
+            dati = str(float(n1) / float(n2))
+
+        print("Ricevuto: '%s'" % dati)
+
+        dati = "Risposta: " + str(addr_client) + ". Il risultato dell'operazione: '" + op + "' tra '" + str(
+            n1) + "' e '" + str(n2) + "' è: '" + dati + "'"
+
+        # codifica la stringa in byte
         dati = dati.encode()
-        # Invia i dati modificati al client
-        cs.send(dati)
-        print('Inviato al client:', dati)
+
+        # Invia la risposta al client.
+        sock_service.send(dati)
+
+    sock_service.close()
