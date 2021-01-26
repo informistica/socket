d1ab4d6308475ef05352496a247cfc875b9cb8ed
informistica <informistica>
2021-01-26 19:54:45 +0100

    Aggiornati client e server

--- server.py
+++ server.py
@@ -9,9 +9,7 @@ SERVER_PORT = 22224
 
 # Crea la socket
 sock_listen = socket.socket()
-op = " "
-n1 = 0
-n2 = 0
+
 # Opzionale: permette di riavviare subito il codice,
 # altrimenti bisognerebbe aspettare 2-4 minuti prima di poter riutilizzare(bindare) la stessa porta
 sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -22,7 +20,7 @@ sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
 # Imposta quante connessioni pendenti possono essere accodate
 sock_listen.listen(5)
 
-print("Server in ascolto su %s. Termina con ko" % str((SERVER_ADDRESS, SERVER_PORT)))
+print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
 
 # Abbiamo creato un endpoint di ascolto (sock_listen) dal quale accettare connessioni in entrata
 # Il loop accetterà una connessione alla volta e la servirà in una nuova connessione (sock_service) finchè il client non si disconnette
@@ -30,34 +28,22 @@ print("Server in ascolto su %s. Termina con ko" % str((SERVER_ADDRESS, SERVER_PO
 
 while True:
     sock_service, addr_client = sock_listen.accept()
-    print("\nConnessione ricevuta da %s" % str(addr_client))
-    print("\nAspetto di ricevere i dati dell'operazione ")
-
+    print("\nConnessione ricevuta da " + str(addr_client))
+    print("\nAspetto di ricevere i dati ")
+    contConn=0
     while True:
         dati = sock_service.recv(2048)
+        contConn+=1
         if not dati:
             print("Fine dati dal client. Reset")
             break
-
         # Decodifica i byte ricevuti in una stringa unicode
         dati = dati.decode()
-        if dati == "ko":
-            print("Fine dati dal client. Exit")
-            break
-        op, n1, n2 = dati.split(";")
-        if op == "piu":
-            dati = str(float(n1) + float(n2))
-        elif op == "meno":
-            dati = str(float(n1) - float(n2))
-        elif op == "per":
-            dati = str(float(n1) * float(n2))
-        elif op == "diviso":
-            dati = str(float(n1) / float(n2))
-
         print("Ricevuto: '%s'" % dati)
-
-        dati = "Risposta: " + str(addr_client) + ". Il risultato dell'operazione: '" + op + "' tra '" + str(
-            n1) + "' e '" + str(n2) + "' è: '" + dati + "'"
+        if dati=='0':
+            print("Chiudo la connessione con " + str(addr_client))
+            break
+        dati = "Risposta a : " + str(addr_client) + ". Il valore del contatore è : " + str(contConn)
 
         # codifica la stringa in byte
         dati = dati.encode()
