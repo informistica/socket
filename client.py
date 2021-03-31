4400d305522363186625729e55684ca6c60217a3
informistica <informistica>
2021-03-31 06:52:14 +0200

    update

--- client.py
+++ client.py
@@ -1,37 +1,32 @@
 #!/usr/bin/env python3
-
 import socket
-import random
-
 SERVER_ADDRESS = '127.0.0.1'
 SERVER_PORT = 22224
-
 sock_service = socket.socket()
-
 sock_service.connect((SERVER_ADDRESS, SERVER_PORT))
 
-print("Connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
-protocollo = ["Send SYN", "Receives SYN ACK","Sends ACK with Data"]
+print("Client connesso a " + str((SERVER_ADDRESS, SERVER_PORT)))
+#              send   receive      send            receive
+protocollo = ["SYN", "SYN ACK","ACK with Data","ACK for Data"]
 step=0
-syn = random.randint(1,1000)
-dati = str(step)+";"+str(syn)
+dati = str(step)
 while True:
     dati = dati.encode()
     sock_service.send(dati)
-    print(protocollo[int(step)])
+    print("Invio: "+ str(step)+ " - " + protocollo[step])
     dati = sock_service.recv(2048)
-    print(dati)
     if not dati:
         print("Server non risponde. Exit")
         break
     dati = dati.decode()
-    step,msg = dati.split(";")
-    if step == '3':
-        print("Termino protocollo")
+    if dati == '3':
+        print("Ricevuto: "+ dati+ " - " + protocollo[int(dati)])
+        print("Termino connessione")
         break
     else:
-        print(str(step)+ protocollo[int(step)])
-       
-    print(dati + '\n')
-
+        step = int(dati)
+        print("Ricevuto: "+ str(step)+ " - " + protocollo[step])
+        step+=1
+        dati = str(step)
+        
 sock_service.close()
--- server.py
+++ server.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
 import socket
-
-
-
 SERVER_ADDRESS = '127.0.0.1'
-
 SERVER_PORT = 22224
-
 sock_listen = socket.socket()
-
 sock_listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 sock_listen.bind((SERVER_ADDRESS, SERVER_PORT))
-
 sock_listen.listen(5)
-
 print("Server in ascolto su %s." % str((SERVER_ADDRESS, SERVER_PORT)))
-
-protocollo=["Receive SYN","Send SYN + ACK", "Receive ACK", "Handshake ok"]
-
+#          receive   send         receive         send 
+protocollo=["SYN","SYN + ACK", "ACK + data", "ACK for Data"]
 while True:
     sock_service, addr_client = sock_listen.accept()
     print("\nConnessione ricevuta da " + str(addr_client))
     print("\nAspetto di ricevere i dati ")
+    step=0
     while True:
         dati = sock_service.recv(2048)
         if not dati:
@@ -30,13 +21,12 @@ while True:
             break
         
         dati = dati.decode()
-        step,msg=dati.split(";")
-        print(f"{protocollo[int(step)]}")
-        if step=='3':
+        print(f"Ricevuto: {dati} - {protocollo[int(dati)]}")
+        if dati=='5':
             print("Fine protocollo  " + str(addr_client))
             break
-        dati = str(int(step)+1)+";"+ protocollo[int(step)+1]
-        print(f"Invio: {dati}")
+        dati = str(int(dati)+1)
+        print(f"Invio: {dati} - {protocollo[int(dati)]}")
         dati = dati.encode()
         sock_service.send(dati)
 
